c642d352c9830b02c696ae72f1fb2c1e81f0f6c7
meissaekawardana <meissaeka66@gmail.com>
2018-10-16 21:12:25 +0700

    RAPIHIN KONTROLLER,MODEL

--- application/controllers/User_controller.php
+++ application/controllers/User_controller.php
@@ -7,60 +7,59 @@ class User_controller extends CI_Controller{
         $this->load->helper(array('form', 'url'));
         $this->load->model('User_model');
     }
+
+    //LOGIN
     function index()
     {
-      $this->load->view('login');
+        $this->load->view('login');
     }
 
     function auth(){
-    $username    = $this->input->post('username');
-    $password    = md5($this->input->post('password'));
+        $username    = $this->input->post('username');
+        $password    = md5($this->input->post('password'));
 
-    $validate = $this->User_model->validate($username,$password);
-    if($validate->num_rows() >0 ) {
-      $data      = $validate->row_array();
-      $username  = $data['username'];
-      $level     = $data['level'];
-      $sesdata = array(
-          'id'        => $data['id'],
-          'username'  => $data['username'],
-          'password'  => $password,
-          'level'     => $level,
-          'logged_in' => TRUE );
-        $this->session->set_userdata($sesdata);       //access login for user
-        if($level === '2'){
+        $validate = $this->User_model->validate($username,$password);
+        if($validate->num_rows() >0 ) {
+            $data      = $validate->row_array();
+            $username  = $data['username'];
+            $level     = $data['level'];
+            $sesdata = array(
+                'id'        => $data['id'],
+                'username'  => $data['username'],
+                'password'  => $password,
+                'level'     => $level,
+                'logged_in' => TRUE );
+            $this->session->set_userdata($sesdata);       //access login for user
+            if($level === '2'){
+                $this->load->view('template/v_header');
+                $this->load->view('template/v_sidebar');
+                $this->load->view('user/Dashboard');
+                $this->load->view('template/v_footer');
+            } else  {
+                $this->load->view('admin/Dashboard');
+            }
+          }
+    }
+
+//DASHBOARD PERTAMA KALI
+    function dash(){
+      if($this->session->userdata('level')== 1){
+          $this->load->view('admin/Dashboard');
+      } else
           $this->load->view('template/v_header');
           $this->load->view('template/v_sidebar');
           $this->load->view('user/Dashboard');
           $this->load->view('template/v_footer');
-        }
-        else  {
-          $this->load->view('admin/Dashboard');
-        }
-      }
     }
 
-    function dash()
-    {
-      if($this->session->userdata('level')== 1){
-        $this->load->view('admin/Dashboard');
-      }
-      else
-        $this->load->view('template/v_header');
-        $this->load->view('template/v_sidebar');
-        $this->load->view('user/Dashboard');
-        $this->load->view('template/v_footer');
+//REGISTRASI USER BARU
+    function insert(){
+          $data['judul'] = 'Insert Data User';
+          $this->load->view('register', $data);
+          $this->load->model('User_model');
     }
 
-    function insert()
-    {
-      $data['judul'] = 'Insert Data User';
-      $this->load->view('register', $data);
-      $this->load->model('User_model');
-    }
-
-    function simpan_user()
-    {
+    function simpan_user() {
           $this->load->model('User_model');
           $this->User_model->simpan_user();
           $data['notifikasi'] = 'Data berhasil disimpan';
@@ -70,185 +69,185 @@ class User_controller extends CI_Controller{
           redirect('User_controller/tampil');
     }
 
-    function tampil()
-    {
-      $this->load->model('User_model');
-      $data['judul'] = 'Menampilkan Data dari Database Menggunakan Codeigniter';
-      $data['daftar_user'] = $this->User_model->get_user_all();
-      if($this->session->userdata('level') == 1){
-      $this->load->view('template/v_header');
-      $this->load->view('template/v_sidebar');
-      $this->load->view('admin/user_list', $data);
-      $this->load->view('template/v_footer');  }
-      else{
+//MENAMPILKAN DATA UNTUK ADMIN
+    function tampil() {
+          $this->load->model('User_model');
+          $data['judul'] = 'Menampilkan Data dari Database Menggunakan Codeigniter';
+          $data['daftar_user'] = $this->User_model->get_user_all();
+          if($this->session->userdata('level') == 1){
+              $this->load->view('template/v_header');
+              $this->load->view('template/v_sidebar');
+              $this->load->view('admin/user_list', $data);
+              $this->load->view('template/v_footer');
+          }else{
         //echo $this->session->userdata('id');
-        $this->load->view('template/v_header');
-        $this->load->view('template/v_sidebar');
-        $this->load->view('user/profil',$data);
-        $this->load->view('template/v_footer');
+              $this->load->view('template/v_header');
+              $this->load->view('template/v_sidebar');
+              $this->load->view('user/profil',$data);
+              $this->load->view('template/v_footer');
+          }
+    }
+
+//HAPUS DATA UNTUK ADMIN
+    function hapus(){
+          $this->load->model('User_model');
+          $data['judul'] = 'Delete Record Menggunakan Codeigniter';
+          $data['daftar_user'] = $this->User_model->get_user_all();
+          $this->load->view('template/v_header');
+          $this->load->view('template/v_sidebar');
+          $this->load->view('admin/daftar_user', $data);
+          $this->load->view('template/v_footer');
+    }
+
+//HAPUS DATA BERDASAR ID
+    function delete_user($id) {
+          $this->load->model('User_model');
+          $username = $this->User_model->delete_user($id);
+          redirect('User_controller/hapus');
+    }
+
+//EDIT BIODATA USER
+    function edit_user() {
+          $this->load->model('User_model');
+          $data['judul'] = 'Menampilkan Data dari Database Menggunakan Codeigniter';
+          $data['daftar_user'] = $this->User_model->get_user_all();
+          if($this->session->userdata('level') == 2 ){
+            $this->load->view('template/v_header');
+            $this->load->view('template/v_sidebar');
+            $this->load->view('user/editprofil',$data);
+            $this->load->view('template/v_footer');
+            }
+    }
+
+//MENYIMPAN EDITAN FORM BIODATA USER
+    function simpan_edit_user($id) {
+            $id=$this->session->userdata('id');
+            $nama = $this->input->post('nama');
+            $level = $this->input->post('level');
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $email = $this->input->post('email');
+            $nim= $this->input->post('nim');
+            $jurusan= $this->input->post('jurusan');
+            $tlp= $this->input->post('tlp');
+
+            $data['judul'] = 'Update Data Codeigniter';
+            $this->load->model('User_model');
+            $data['edit'] = $this->User_model->simpan_edit_user( $nama,$level, $username, $password, $email, $nim,$jurusan,$tlp);
+            $data['notifikasi'] = 'Data telah berhasil disimpan';
+            $this->load->view('notifikasi', $data);
+            redirect('User_controller/tampil');
+    }
+
+//MELOAD 6 MACAM THREAD
+    function thread($id_thread=''){
+            $id_thread=$this->uri->segment(3);
+            if(!isset($id_thread)){
+                $id_thread=1;
+            } else {
+                $id_thread=$id_thread;  }
+            $this->load->model('User_model');
+            $data['id_thread']=$id_thread;
+            $data['thread']=$this->User_model->thread();
+            $data['id_thread']=$id_thread;
+            $data['forumDisplayFill']=$this->User_model->getForumFill($id_thread);
+
+            $this->load->view('template/v_header');
+            $this->load->view('template/v_sidebar');
+            $this->load->view('timeline/Dashboard',$data);
+            $this->load->view('template/v_footer');
+    }
+
+//MENAMBAH POST/TIMELINE BARU
+    function tambahtimeline($id_thread=''){
+            $data['id']=$id_thread;
+            $data['thread']=$this->User_model->thread();
+            $info=$this->User_model->getProp($id_thread);
+            if(!empty($info->namaforum)){
+                $data['namaforum']=$info->namaforum; }
+                $this->load->view('template/v_header');
+                $this->load->view('template/v_sidebar');
+                $this->load->view('timeline/addpost',$data);
+                $this->load->view('template/v_footer');
+    }
+
+    //MENYIMPAN TIMELINE BARU
+        function simpantimeline(){
+                $this->load->model('User_model');
+                $id_thread=$this->input->post('id_thread');
+                $judul=$this->input->post('judul');
+                $isi=$this->input->post('isi');
+          		  $type=$this->input->post('type');
+          		  $matkul=$this->input->post('matkul');
+          		  $datementor=$this->input->post('datementor');
+          		  $deadline=$this->input->post('deadline');
+          		  $feemin=$this->input->post('feemin');
+          		  $feemax=$this->input->post('feemax');
+
+                $this->User_model->saveThread();
+                $data['notifikasi'] = 'Data berhasil disimpan';
+                $data['judul']='Insert Timeline Berhasil';
+                $this->load->view('notifikasi', $data);
+                //redirect('User_controller/tampiltimeline');
         }
-      }
 
-    function hapus()
-    {
-        $this->load->model('User_model');
-        $data['judul'] = 'Delete Record Menggunakan Codeigniter';
-        $data['daftar_user'] = $this->User_model->get_user_all();
-        $this->load->view('template/v_header');
-        $this->load->view('template/v_sidebar');
-        $this->load->view('admin/daftar_user', $data);
-        $this->load->view('template/v_footer');
+//MELOAD DETAIL SUATU POST
+    function detailthread($id_timeline='')  {
+            $id_thread=$this->uri->segment(3);
+            $data['id_thread']=$id_thread;
+            $id_timeline=$this->uri->segment(4);
+            $data['id_timeline']=$id_timeline;
+            $data['forumDisplayFill']=$this->User_model->getDetailForum($id_timeline);
+            $data['forumDisplayFillComment']=$this->User_model->getFcontentComment($id_timeline);
+            $data['thread']=$this->User_model->thread();
+            $this->load->view('template/v_header');
+            $this->load->view('template/v_sidebar');
+            $this->load->view('timeline/detail',$data);
+            $this->load->view('template/v_footer');
     }
 
-    function delete_user($id)
-    {
-        $this->load->model('User_model');
-        $username = $this->User_model->delete_user($id);
-        redirect('User_controller/hapus');
-    }
-
-    function edit_user()
-    {
-      $this->load->model('User_model');
-      $data['judul'] = 'Menampilkan Data dari Database Menggunakan Codeigniter';
-      $data['daftar_user'] = $this->User_model->get_user_all();
-      if($this->session->userdata('level') == 2 ){
-        $this->load->view('template/v_header');
-        $this->load->view('template/v_sidebar');
-        $this->load->view('user/editprofil',$data);
-        $this->load->view('template/v_footer');
-        }
-      }
-
-    function simpan_edit_user($id)
-    {
-        $id=$this->session->userdata('id');
-        $nama = $this->input->post('nama');
-        $level = $this->input->post('level');
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        $email = $this->input->post('email');
-        $nim= $this->input->post('nim');
-        $jurusan= $this->input->post('jurusan');
-        $tlp= $this->input->post('tlp');
-
-        $data['judul'] = 'Update Data Codeigniter';
-        $this->load->model('User_model');
-        $data['edit'] = $this->User_model->simpan_edit_user( $nama,$level, $username, $password, $email, $nim,$jurusan,$tlp);
-        $data['notifikasi'] = 'Data telah berhasil disimpan';
-        $this->load->view('notifikasi', $data);
-        redirect('User_controller/tampil');
-    }
-
-    function logout(){
-          $this->session->sess_destroy();
-          redirect('User_controller');
-      }
-
-    function thread($id_thread='')
-    {
-      $id_thread=$this->uri->segment(3);
-      if(!isset($id_thread)){
-        $id_thread=1;
-      }
-      else {
-        $id_thread=$id_thread;
-      }
-      $this->load->model('User_model');
-      $data['id_thread']=$id_thread;
-      $data['thread']=$this->User_model->thread();
-      $data['id_thread']=$id_thread;
-      $data['forumDisplayFill']=$this->User_model->getForumFill($id_thread);
-
-      $this->load->view('template/v_header');
-      $this->load->view('template/v_sidebar');
-      $this->load->view('timeline/Dashboard',$data);
-      $this->load->view('template/v_footer');
-    }
-
-    function tambahtimeline($id_thread='')
-    {
-
-      $data['id']=$id_thread;
-      $data['thread']=$this->User_model->thread();
-      $info=$this->User_model->getProp($id_thread);
-      if(!empty($info->namaforum)){
-        $data['namaforum']=$info->namaforum; }
-      $this->load->view('template/v_header');
-      $this->load->view('template/v_sidebar');
-      $this->load->view('timeline/addpost',$data);
-      $this->load->view('template/v_footer');
-    }
-
-    function detailthread($id_timeline='')
-    {
-      $id_thread=$this->uri->segment(3);
-      $data['id_thread']=$id_thread;
-      $id_timeline=$this->uri->segment(4);
-      $data['id_timeline']=$id_timeline;
-      $data['forumDisplayFill']=$this->User_model->getDetailForum($id_timeline);
-      $data['forumDisplayFillComment']=$this->User_model->getFcontentComment($id_timeline);
-      $data['thread']=$this->User_model->thread();
-      $this->load->view('template/v_header');
-      $this->load->view('template/v_sidebar');
-      $this->load->view('timeline/detail',$data);
-      $this->load->view('template/v_footer');
-    }
-
-    function simpantimeline()
-    {
-      $this->load->model('User_model');
-      $id_thread=$this->input->post('id_thread');
-      $judul=$this->input->post('judul');
-      $isi=$this->input->post('isi');
-		  $type=$this->input->post('type');
-		  $matkul=$this->input->post('matkul');
-		  $datementor=$this->input->post('datementor');
-		  $deadline=$this->input->post('deadline');
-		  $feemin=$this->input->post('feemin');
-		  $feemax=$this->input->post('feemax');
-
-      $this->User_model->saveThread();
-      $data['notifikasi'] = 'Data berhasil disimpan';
-      $data['judul']='Insert Timeline Berhasil';
-      $this->load->view('notifikasi', $data);
-      //redirect('User_controller/tampiltimeline');
-    }
-
+//MENAMBAH KOMEN DI SUATU POST
     function reqComment($id_thread='',$id_timeline=''){
-    $this->load->model('User_model');
-    $id_thread=$this->uri->segment(3);
-    $data['id_thread']=$id_thread;
-    $id_timeline=$this->uri->segment(4);
-    $data['id_timeline']=$id_timeline;
-    $data['thread']=$this->User_model->thread();
-    $info=$this->User_model->getProp($id_timeline);
-    if(!empty($info->judul)){
-    $data['judul']=$info->judul;}
+            $this->load->model('User_model');
+            $id_thread=$this->uri->segment(3);
+            $data['id_thread']=$id_thread;
+            $id_timeline=$this->uri->segment(4);
+            $data['id_timeline']=$id_timeline;
+            $data['thread']=$this->User_model->thread();
+            $info=$this->User_model->getProp($id_timeline);
+            if(!empty($info->judul)){
+            $data['judul']=$info->judul;}
 
-    $this->load->view('template/v_header');
-    $this->load->view('timeline/addkomen',$data);
-    $this->load->view('template/v_footer');
+            $this->load->view('template/v_header');
+            $this->load->view('timeline/addkomen',$data);
+            $this->load->view('template/v_footer');
     }
 
+//MENYIMPAN KOMEN DI SUATU POST
     function saveComment(){
-    $this->load->model('User_model');
-    $id_timeline=$this->input->post('id_timeline');
-    $isi=$this->input->post('isi');
-    if($isi==''){
-      $data['flashdata']="ISI KOMENTAR TIDAK BOLEH KOSONG";
-      $data['id_timeline']=$id_timeline;
-      $data['thread']=$this->forummodel->thread();
-      $info=$this->User_model->getProp($id_timeline);
-      $data['judul']=$info->judul;
-      $data['id_timeline']=$info->idtimeline;
-      $this->load->view('template/v_header');
-      $this->load->view('timeline/addkomen',$data);
-      $this->load->view('template/v_footer');
+            $this->load->model('User_model');
+            $id_timeline=$this->input->post('id_timeline');
+            $isi=$this->input->post('isi');
+            if($isi==''){
+                  $data['flashdata']="ISI KOMENTAR TIDAK BOLEH KOSONG";
+                  $data['id_timeline']=$id_timeline;
+                  $data['thread']=$this->forummodel->thread();
+                  $info=$this->User_model->getProp($id_timeline);
+                  $data['judul']=$info->judul;
+                  $data['id_timeline']=$info->idtimeline;
+                  $this->load->view('template/v_header');
+                  $this->load->view('timeline/addkomen',$data);
+                  $this->load->view('template/v_footer');
 
-    } else {
-    $this->load->model('User_model');
-    $this->User_model->saveComment($id_thread,$id_timeline);
+            } else {
+                  $this->load->model('User_model');
+                  $this->User_model->saveComment($id_thread,$id_timeline);
+            }
     }
-  }
-  }
+
+    //FUNTION LOGOUT USER
+        function logout(){
+                $this->session->sess_destroy();
+                redirect('User_controller');
+          }
+}
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,6 +1,6 @@
 <?php
 class User_model extends CI_Model{
-
+//VALIDATE AWAL LOGIN
   function validate($username,$password){
     $this->db->where('username',$username);
     $this->db->where('password',$password);
@@ -14,8 +14,8 @@ class User_model extends CI_Model{
    // }
 }
 
-  function simpan_user()
-    {
+//MENYIMPAN REGISTER USER BARU
+  function simpan_user(){
         $simpan_data = array(
             'level'         => 2,
             'nama'          => $this->input->post('nama'),
@@ -25,43 +25,56 @@ class User_model extends CI_Model{
             'email'         => $this->input->post('email'),
             'jurusan'       => $this->input->post('jurusan'),
             'tlp'           => $this->input->post('tlp'),
-            'status'        => 1
-       );
+            'status'        => 1 );
         $simpan = $this->db->insert('user', $simpan_data);
         return $simpan;
     }
 
-    function getForumFillcount($id_thread=''){
-			$query=$this->db->query("select  * from timeline where id_thread='$id_thread'");
-			return $query->num_rows();
-	}
-
-    function get_user_all()
-    {
+//MELOAD SEMUA DATA USER
+    function get_user_all(){
         $query=$this->db->query("SELECT * FROM user ORDER BY id DESC");
         return $query->result();
     }
 
-    function delete_user($id)
-    {
+//MENYIMPAN EDITAN BIODATA USER
+        function simpan_edit_user( $nama,$level, $username, $password, $email, $nim,$jurusan,$tlp){
+            $data = array(
+                'nama'           => $nama,
+                'username'       => $username,
+                'password'       => $password,
+                'email'          => $email,
+                'nim'            => $nim,
+                'jurusan'        => $jurusan,
+                'tlp'            => $tlp  );
+            $this->db->where('id', $this->session->userdata('id'));
+            $this->db->update('user', $data);
+        }
+
+//MELOAD ISI TIMELINE DI SETIAP THREAD
+    function getForumFillcount($id_thread=''){
+  			$query=$this->db->query("select  * from timeline where id_thread='$id_thread'");
+  			return $query->num_rows();
+	  }
+
+//MENGHAPUS SUATU USER BERDASAR ID
+    function delete_user($id){
         $query=$this->db->delete('user',array('id'=>$id));
     }
 
-    function simpan_edit_user( $nama,$level, $username, $password, $email, $nim,$jurusan,$tlp)
-    {
-        $data = array(
-            'nama'           => $nama,
-            'username'       => $username,
-            'password'       => $password,
-            'email'          => $email,
-            'nim'            => $nim,
-            'jurusan'        => $jurusan,
-            'tlp'            => $tlp
-        );
-        $this->db->where('id', $this->session->userdata('id'));
-        $this->db->update('user', $data);
-    }
+//MELOAD DATA TABEL THREAD UNTUK AMBIL NAMANYA
+    function thread($id_thread='')
+      {
+        $query = $this->db->query("select  * from thread
+        ORDER BY id_thread ASC");
+        if ($query->num_rows() > 0) {
+        foreach ($query->result() as $data) {
+          $menus[]=$data;
+         }
+        return $menus;
+         }
+       }
 
+//MENYIMPAN SUATU POST KE DB
     function saveThread(){
         $id_thread=$this->input->post('id_thread');
         $isi=$this->input->post('isi');
@@ -74,73 +87,62 @@ class User_model extends CI_Model{
         $feemax=$this->input->post('feemax');
 
         $data=array(
-        'id_thread'=>$id_thread,
-        'id'=>$this->session->userdata('id'),
-        'isi'=>$isi,
-        'judul'=>$judul,
-        'type'=>$type,
-        'matkul'=>$matkul,
-        'datementor'=>$datementor,
-        'deadline'=>$deadline,
-        'feemin'=>$feemin,
-        'feemax'=>$feemax,
-        'tanggal'=>date("Y-m-d")
-        );
-        $this->db->trans_start();
-        $this->db->insert('timeline',$data);
-        $this->db->trans_complete();
-        redirect (base_url().'User_controller/thread/'.$id_timeline);
+            'id_thread'=>$id_thread,
+            'id'=>$this->session->userdata('id'),
+            'isi'=>$isi,
+            'judul'=>$judul,
+            'type'=>$type,
+            'matkul'=>$matkul,
+            'datementor'=>$datementor,
+            'deadline'=>$deadline,
+            'feemin'=>$feemin,
+            'feemax'=>$feemax,
+            'tanggal'=>date("Y-m-d")  );
+          $this->db->trans_start();
+          $this->db->insert('timeline',$data);
+          $this->db->trans_complete();
+          redirect (base_url().'User_controller/thread/'.$id_timeline);
       }
 
+//MELOAD DETAI DARI SUATU POST YG DI KLIK
     function getDetailForum($id_timeline=''){
-  	$query=$this->db->query("select * ,user.nama from timeline
-  			left join user on user.id=timeline.id
-  			where timeline.id_timeline='$id_timeline' ");
-  			 if ($query->num_rows() > 0) {
-  				foreach ($query->result() as $data) {
-  					$mdata[]=$data;
-  				}
-  				return $mdata;
-  			}
+      	$query=$this->db->query("select * ,user.nama from timeline left join user on user.id=timeline.id where timeline.id_timeline='$id_timeline' ");
+      			 if ($query->num_rows() > 0) {
+      				 foreach ($query->result() as $data) {
+      					 $mdata[]=$data;
+      			 }
+      				 return $mdata;
+      			}
   	}
-    function thread($id_thread='')
-      {
-			  $query = $this->db->query("select  * from thread
-			  ORDER BY id_thread ASC");
-			  if ($query->num_rows() > 0) {
-				foreach ($query->result() as $data) {
-					$menus[]=$data;
-				 }
-				return $menus;
-			   }
-	     }
 
+//MELOAD SEMUA ISI TIMELINE
     function getForumFill($id_thread=''){
-      $id=$this->session->userdata('username');
-			$query=$this->db->query("select *,user.nama,timeline.id_timeline as id_timeline from timeline left join user on user.id=timeline.id where id_thread='$id_thread' ORDER BY timeline.id_timeline ASC");
-			 if ($query->num_rows() > 0) {
-				foreach ($query->result() as $data) {
-					$mdata[]=$data;
-				}
-				return $mdata;
-			}
+          $id=$this->session->userdata('username');
+    			$query=$this->db->query("select *,user.nama,timeline.id_timeline as id_timeline from timeline left join user on user.id=timeline.id where id_thread='$id_thread' ORDER BY timeline.id_timeline ASC");
+    			 if ($query->num_rows() > 0) {
+    				  foreach ($query->result() as $data) {
+    					  $mdata[]=$data;
+    			    }
+    			 	 return $mdata;
+    			 }
 	   }
 
+//MELOAD TABEL TIMELINE
      function getProp($id_timeline=''){
-	      $query=$this->db->query("select * ,thread.nama as nama from timeline
-	      left join thread on thread.id_thread=timeline.id_timeline
-	      where id_timeline='$id_timeline'");
-	      return $query->row();
+  	      $query=$this->db->query("select * ,thread.nama as nama from timeline
+  	      left join thread on thread.id_thread=timeline.id_timeline
+  	      where id_timeline='$id_timeline'");
+  	      return $query->row();
       }
 
+//MENYIMPAN SUATU KOMEN
       function saveComment($id_timeline=''){
       		$isi=$this->input->post('isi');
       		$data=array(
-      		'id_timeline'=>$id_timeline,
-      		'id'=>$this->session->userdata('id'),
-      		'isi'=>$isi,
-      		'tanggal'=>date("Y-m-d")
-      		);
+          		'id_timeline'=>$id_timeline,
+          		'id'=>$this->session->userdata('id'),
+          		'isi'=>$isi,
+          		'tanggal'=>date("Y-m-d")	);
       		$this->db->trans_start();
       		$this->db->insert('komentar',$data);
       		$this->db->trans_complete();
